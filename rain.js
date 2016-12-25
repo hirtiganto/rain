f6e48ef60b195208c1f15b019fa2de1c42dae0a6
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-25 16:58:21 +0100

    Added render function and code that maps rain's velocity to it's size and so on

--- rain.js
+++ rain.js
@@ -1,3 +1,12 @@
 function Rain() {
-  
+  this.location = createVector(random(0,width),random(0,height));
+  this.gravity = createVector(0,random(5,15));
+
+  this.render = function() {
+    stroke(255)
+    strokeWeight(map(this.gravity.y,5,15,3,7));
+    
+    var l = map(this.gravity.y,5,15,10,25);
+    line(this.location.x,this.location.y,this.location.x,this.location.y + l);
+  }
 }

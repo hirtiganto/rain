53b5df517246f8a031b89942f0c129822ac6e9cd
Jakub Pustelnik <jhirtiganto@gmail.com>
2016-12-25 16:58:54 +0100

    Added code that renders rain

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
+var rain = [];
+
 function setup() {
-  createCanvas(windowWidht,windowHeight);
+  createCanvas(windowWidth,windowHeight);
+  for (var i = 0; i < 200; i++) {
+    rain[i] = new Rain();
+  }
 }
 
 function draw() {
   background(0);
+  for (var i = 0; i < rain.length; i++) {
+    rain[i].render();
+  }
 }
